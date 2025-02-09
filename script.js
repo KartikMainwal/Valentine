169bfd0829b3840401acb7c6f3d3157902878948
KartikMainwal <156320225+KartikMainwal@users.noreply.github.com>
2025-02-09 16:30:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Are you positive?",
+    "Pookie please...",
+    "Just think about it!",
+    "If you say no, I will be really sad...",
+    "I will be very sad...",
+    "I will be very very very sad...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+const maxClicks = 8;
+
+function handleNoClick() {
+    messageIndex++;
+    const noButton = document.querySelector('.no-button');
+    if (messageIndex >= maxClicks) {
+        window.location.href = 'no_page.html';
+    }
+
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 0.6) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+
+}
